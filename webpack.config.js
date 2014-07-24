0a216cd6ebfdee62bf901d89a0d3df6c76538c3f
Nicolas Gallagher <nicolasgallagher@gmail.com>
2014-07-24 11:58:51 -0700

    Remove unused require in webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,5 @@
 var BannerPlugin = require('webpack/lib/BannerPlugin');
 var version = require('./package.json').version;
-var webpack = require('webpack');
 
 module.exports = {
   entry: './index',
